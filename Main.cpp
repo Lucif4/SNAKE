40220e94e1a467d8d0735d3154b89033b97a678d
Lucifer <88179705+Lucif4@users.noreply.github.com>
2021-09-12 17:39:09 +0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include "Game.h"
+
+int main(int argc, char* argv[]){
+	// Program entry point.
+	Game game;
+	while(!game.GetWindow()->IsDone()){
+		game.HandleInput();
+		game.Update();
+		game.Render();
+		game.RestartClock();
+	}
+}
+
+
